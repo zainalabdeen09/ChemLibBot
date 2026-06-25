8dbf3e62574de76c2b0fe376eefa25475d88df24
zainalabdeen09 <zainalabdeen09@users.noreply.github.com>
2026-06-26 01:20:37 +0300

    Fix Railway config: web process, health check, Python 3.12 pin

--- src/config.py
+++ src/config.py
@@ -1,2 +1,4 @@
-TOKEN = "8846362609:AAFnlf-vvXZ6khzLy59t_XSmJGrLvrscfJg"
-ADMIN_USERNAME = "@za_c10"
+import os
+
+TOKEN = os.environ.get("TOKEN", "8846362609:AAFnlf-vvXZ6khzLy59t_XSmJGrLvrscfJg")
+ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "@za_c10")
